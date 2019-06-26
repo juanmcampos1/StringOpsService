5e661e89ae752f2a34afed9128d2bf99b24d26fa
Juan M. Campos <jcampos@alidasoftware.com>
2019-06-26 09:49:49 -0500

    2019_2.0
    Ver_012020

---
+++ src/test/java/com/aurea/cx/messenger/example/StringOpsService/StringOpsServiceTest.java
@@ -0,0 +1,34 @@
+package com.aurea.cx.messenger.example.StringOpsService;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * StringOpsService Test Class
+ */
+public class StringOpsServiceTest {
+
+    private StringOpsService service;
+
+    @Before
+    public void setUp() throws Exception {
+        service = new StringOpsService();
+    }
+
+    @Test
+    public void concatenateTest() {
+        assertEquals("ABC:DEF", service.concatenate(new String[] {"ABC", "DEF"}, ":"));
+    }
+
+    @Test
+    public void splitTest() {
+        assertEquals(3, service.split("A:BC:DEF", ":").length);
+    }
+
+    @Test
+    public final void convertTest() {
+        assertEquals(22.22f, service.convert(72), 0.1);
+    }
+}
